582cd4b478e37de0208f8e44f7adc978e226ce67
mcwindy <40684304+mcwindy@users.noreply.github.com>
2023-03-16 15:14:44 +0800

    Initial upload

---
+++ pttime.py
@@ -0,0 +1,35 @@
+import requests
+import time
+
+from selenium import webdriver
+
+with open('.env') as f:
+    username, password, push_url = f.read().split('\n')[:3]
+try:
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(executable_path='./geckodriver', options=options)
+    url = 'https://www.pttime.org/login.php?returnto=attendance.php'
+    driver.get(url)
+    driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[1]/td[2]/input').send_keys(username)
+    driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[2]/td[2]/input').send_keys(password)
+    driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[4]/td[2]/input').click()
+    driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[7]/td/input[1]').click()
+
+    time.sleep(5)
+    days = driver.find_element('xpath', '/html/body/table[4]/tbody/tr/td/table/tbody/tr/td/span[3]/b').text
+    share_ratio = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[1]').text
+    upload_amount = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[2]').text
+    download_amount = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[3]').text
+    magic_points_long = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[7]').text
+
+    driver.close()
+except:
+    requests.post(push_url, data=f'[PTTime] failed, {username}')
+else:
+    import re
+    magic_points = re.match('魔力值 \((.*?)魔力/小时\) \[使用\&说明\]: (.*?)\(获得(.*?),详情\)', magic_points_long).groups()
+    requests.post(
+        push_url,
+        data=
+        f'[PTTime] succeeded, get {magic_points[2]} magic points, {username} has continuously checked in for {days} days, possessing {magic_points[1]} magic points'
+    )
