b7e8bec5103d1845bd4243c96fe0e744acdb5805
mcwindy <40684304+mcwindy@users.noreply.github.com>
2023-05-04 01:08:26 +0800

    Update pttime.py

--- pttime.py
+++ pttime.py
@@ -18,13 +18,14 @@ try:
     driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[4]/td[2]/input').click()
     driver.find_element('xpath', '//*[@id="nav_block"]/form[2]/table/tbody/tr[7]/td/input[1]').click()
 
-    time.sleep(5)
+    time.sleep(3)
+    driver.refresh()
+    time.sleep(3)
     days = driver.find_element('xpath', '/html/body/table[4]/tbody/tr/td/table/tbody/tr/td/span[3]/b').text
     share_ratio = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[1]').text
     upload_amount = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[2]').text
     download_amount = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[3]').text
     magic_points_long = driver.find_element('xpath', '/html/body/table[2]/tbody/tr/td/table[2]/tbody/tr[2]/td/div[1]/span[7]').text
-
     driver.close()
 
     magic_points = re.match('魔力值 \((.*?)魔力/小时\) \[使用\&说明\]: (.*?)\(获得(.*?),详情\)', magic_points_long).groups()
