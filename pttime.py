97e16e7c5ffc1084f3c084b63da9f53de85c00ce
mcwindy <mcwindy0@outlook.com>
2024-03-07 17:32:24 +0800

    1

--- pttime.py
+++ pttime.py
@@ -6,11 +6,59 @@ import traceback
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 browser = 'firefox'  # 'firefox' or 'chrome'
 
+def get_driver(browser):
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument("--remote-debugging-port=9223")
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        options.add_experimental_option("prefs", prefs)
+    elif browser == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--shm-size 2g")
+    if not options:
+        return
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
 
-def do_checkin(browser):
+    # options.add_argument(
+    #     "User-Agent=Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BLA-AL00 Build/HUAWEIBLA-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.2987.132 MQQBrowser/8.9 Mobile Safari/537.36"
+    # )
+
+
+    if browser == "chrome":
+        # driver = webdriver.Chrome(
+        #     service=ChromiumService(
+        #         ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        #     ),
+        #     options=options,
+        # )
+        # hub_url = "http://127.0.0.1:4444"
+        hub_url = "http://10.160.34.251:4444"
+        driver = webdriver.Remote(
+            command_executor=hub_url,
+            options=options,
+        )
+        # driver = webdriver.Chrome(
+        #     service=ChromiumService(executable_path="./chromedriver"),
+        #     options=options,
+        # )
+    elif browser == "firefox":
+        # driver = webdriver.Firefox(
+        #     service=FirefoxService(GeckoDriverManager().install()),
+        #     options=options,
+        # )
+        driver = webdriver.Firefox(
+            service=FirefoxService(executable_path="./geckodriver", log_output='./geckodriver.log'),
+            options=options,
+        )
+    return driver
+
+def old_get_driver(browser):
     if browser == 'chrome':
         options = webdriver.ChromeOptions()
         options.add_argument('--remote-debugging-port=9222')
@@ -25,6 +73,8 @@ def do_checkin(browser):
     elif browser == 'firefox':
         service = Service(current_dir + '/geckodriver')
         driver = webdriver.Firefox(service=service, options=options)
+
+def do_checkin(driver):
     url = 'https://www.pttime.org/login.php?returnto=attendance.php'
     driver.get(url)
     driver.find_element('xpath', '//*[@id="nav_block"]/form/table/tbody/tr[1]/td[2]/input').send_keys(username)
@@ -55,12 +105,13 @@ print(current_dir)
 with open(current_dir + '/.env') as f:
     username, password, push_url = f.read().split('\n')[:3]
 
-try_count = 3
+try_count = 2
 
 if __name__ == '__main__':
+    driver = get_driver(browser)
     for i in range(try_count):
         try:
-            days, magic_points = do_checkin(browser)
+            days, magic_points = do_checkin(driver)
         except Exception as e:
             traceback.print_exc()
         else:
